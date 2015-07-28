bdfe9477779fe1f4fa965d3d3b12c4c8245ed1ec
Alexis Guerville <alexis.guerville@noparking.net>
2015-07-28 12:50:11 +0200

    Fix token registration

--- register.php
+++ register.php
@@ -8,8 +8,7 @@ if (!empty($webhook)) {
         $req = $db->prepare("SELECT COUNT(*) FROM tokens WHERE user_id = ?");
         $req->execute(array($webhook['user_id']));
         $get_token = $req->fetchColumn();
-        error_log($get_token);
-        if ($get_token) {
+        if ($get_token > 0) {
             $req = $db->prepare("INSERT INTO tokens (user_id, app_id, token) VALUES(:user_id, :app_id, :token)");
             $req->execute(array(
                 'user_id' => $webhook['user_id'],
