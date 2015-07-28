0181fb48ad9dc117891adb022dbbc5f0e367a208
Alexis Guerville <alexis.guerville@noparking.net>
2015-07-28 12:48:23 +0200

    Fix token registration

--- register.php
+++ register.php
@@ -5,9 +5,9 @@ include("setup.php");
 $webhook = json_decode(file_get_contents('php://input', 'rb'), true);
 if (!empty($webhook)) {
     foreach ($webhook['_push']['android_tokens'] as $token) {
-        $req = $db->prepare("SELECT token FROM tokens WHERE user_id = ?");
+        $req = $db->prepare("SELECT COUNT(*) FROM tokens WHERE user_id = ?");
         $req->execute(array($webhook['user_id']));
-        $get_token = $req->rowCount();
+        $get_token = $req->fetchColumn();
         error_log(print_r($get_token));
         if ($get_token) {
             $req = $db->prepare("INSERT INTO tokens (user_id, app_id, token) VALUES(:user_id, :app_id, :token)");
